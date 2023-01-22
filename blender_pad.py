cedefeee544fdc450e7340974402c135d9e1d5ce
Ronald Sims II <tace105@crushgroove.madtown>
2023-01-22 17:40:33 -0500

    Added short cut screen to blender pad for 60% keyboards addid dedicated arrows, tilde and back tick.

--- blender_pad.py
+++ blender_pad.py
@@ -18,6 +18,9 @@ keys = pmk.keys
 mode = 0
 # apple = 0, linux = 1, windows = 2
 comp_os = 0
+# select pad layout
+screen = 0
+arrow_keys = [6, 5, 1, 9]  # up, down, left right
 
 # X color
 red = (245, 66, 66)
@@ -31,19 +34,29 @@ blue = (66, 135, 245)
 yellow = (245, 233, 66)
 white = (255, 255, 255)
 
-# Turn on lights (grab)
-keys[3].set_led(*red)
-keys[7].set_led(*green)
-keys[11].set_led(*blue)
-# Turn on lights (rotate)
-keys[2].set_led(*red)
-keys[6].set_led(*green)
-keys[10].set_led(*blue)
-# Turn on lights (scale)
-keys[1].set_led(*red)
-keys[5].set_led(*green)
-keys[9].set_led(*blue)
+def lights_on():
+    # Turn on lights (grab)
+    keys[3].set_led(*red)
+    keys[7].set_led(*green)
+    keys[11].set_led(*blue)
+    # Turn on lights (rotate)
+    keys[2].set_led(*red)
+    keys[6].set_led(*green)
+    keys[10].set_led(*blue)
+    # Turn on lights (scale)
+    keys[1].set_led(*red)
+    keys[5].set_led(*green)
+    keys[9].set_led(*blue)
 
+
+lights_on()
+
+def arrow_keys_toggle(on=False):
+    for x in arrow_keys:
+        if on:
+            keys[x].set_led(*blue)
+        else:
+            keys[x].led_off()
 
 def safe_sleep(s):
     clock = 0
@@ -109,112 +122,219 @@ def comp_set(key):
     if comp_os >= 3:
         comp_os = 0
 
+
+# pad layout button
+@pmk.on_press(keys[12])
+def screen_set(key):
+    global screen
+    screen += 1
+    if screen > 0:
+        for x in range(16):
+            if x % 4 != 0:
+                keys[x].led_off()
+                arrow_keys_toggle(True)
+    if screen >= 2:
+        arrow_keys_toggle()
+        lights_on()
+        screen = 0
+
 # grab
 @pmk.on_press(keys[3])
 def grab_x(key):
-    grab(key, 'x')
-    safe_sleep(0.3)
-    key.set_led(*red)
+    if screen == 0:
+        grab(key, 'x')
+        safe_sleep(0.3)
+        key.set_led(*red)
+    if screen == 1:
+        pass
 
 
 @pmk.on_press(keys[7])
 def grab_y(key):
-    grab(key, 'y')
-    safe_sleep(0.3)
-    key.set_led(*green)
+    if screen == 0:
+        grab(key, 'y')
+        safe_sleep(0.3)
+        key.set_led(*green)
+    if screen == 1:
+        pass
 
 
 @pmk.on_press(keys[11])
 def grab_y(key):
-    grab(key, 'z')
-    safe_sleep(0.3)
-    key.set_led(*blue)
+    if screen == 0:
+        grab(key, 'z')
+        safe_sleep(0.3)
+        key.set_led(*blue)
+    if screen == 1:
+        pass
 
 
 # rotate
 @pmk.on_press(keys[2])
 def rotate_y(key):
-    rotate(key, 'x')
-    safe_sleep(0.3)
-    key.set_led(*red)
+    if screen == 0:
+        rotate(key, 'x')
+        safe_sleep(0.3)
+        key.set_led(*red)
+    if screen == 1:
+        pass
 
 
 @pmk.on_press(keys[6])
 def rotate_y(key):
-    rotate(key, 'y')
-    safe_sleep(0.3)
-    key.set_led(*green)
+    if screen == 0:
+        rotate(key, 'y')
+        safe_sleep(0.3)
+        key.set_led(*green)
+    if screen == 1:
+        pass
 
 
 @pmk.on_press(keys[10])
 def rotate_y(key):
-    rotate(key, 'z')
-    safe_sleep(0.3)
-    key.set_led(*blue)
+    if screen == 0:
+        rotate(key, 'z')
+        safe_sleep(0.3)
+        key.set_led(*blue)
+    if screen == 1:
+        pass
 
 
 # scale
 @pmk.on_press(keys[1])
 def scale_x(key):
-    scale(key, 'x')
-    safe_sleep(0.3)
-    key.set_led(*red)
+    if screen == 0:
+        scale(key, 'x')
+        safe_sleep(0.3)
+        key.set_led(*red)
+    if screen == 1:
+        pass
 
 
 @pmk.on_press(keys[5])
 def scale_y(key):
-    scale(key, 'y')
-    safe_sleep(0.3)
-    key.set_led(*green)
+    if screen == 0:
+        scale(key, 'y')
+        safe_sleep(0.3)
+        key.set_led(*green)
+    if screen == 1:
+        pass
 
 
 @pmk.on_press(keys[9])
 def scale_z(key):
-    scale(key, 'z')
-    safe_sleep(0.3)
-    key.set_led(*blue)
+    if screen == 0:
+        scale(key, 'z')
+        safe_sleep(0.3)
+        key.set_led(*blue)
+    if screen == 1:
+        pass
 
 
 # reset
 @pmk.on_press(keys[15])
 def reset_grab(key):
-    key.set_led(*white)
-    keyboard.send(Keycode.ESCAPE, get_alt(comp_os), Keycode.G)
-    safe_sleep(0.3)
-    key.led_off()
+    if screen == 0:
+        key.set_led(*white)
+        keyboard.send(Keycode.ESCAPE, get_alt(comp_os), Keycode.G)
+        safe_sleep(0.3)
+        key.led_off()
+    if screen == 1:
+        pass
 
 
 @pmk.on_press(keys[14])
 def reset_rotate(key):
-    key.set_led(*white)
-    keyboard.send(Keycode.ESCAPE, get_alt(comp_os), Keycode.R)
-    safe_sleep(0.3)
-    key.led_off()
+    if screen == 0:
+        key.set_led(*white)
+        keyboard.send(Keycode.ESCAPE, get_alt(comp_os), Keycode.R)
+        safe_sleep(0.3)
+        key.led_off()
+    if screen == 1:
+        pass
 
 
 @pmk.on_press(keys[13])
 def reset_scale(key):
-    key.set_led(*white)
-    keyboard.send(Keycode.ESCAPE, get_alt(comp_os), Keycode.S)
-    safe_sleep(0.3)
-    key.led_off()
+    if screen == 0:
+        key.set_led(*white)
+        keyboard.send(Keycode.ESCAPE, get_alt(comp_os), Keycode.S)
+        safe_sleep(0.3)
+        key.led_off()
+    if screen == 1:
+        pass
+
+
+@pmk.on_press(keys[3])
+def send_tilde(key):
+    if screen == 1:
+        layout.write('~')
+
+
+@pmk.on_press(keys[7])
+def send_backtick(key):
+    if screen == 1:
+        layout.write('`')
+
+
+def arrow(key, d):
+    dir = {
+        'u': Keycode.UP_ARROW,
+        'd': Keycode.DOWN_ARROW,
+        'l': Keycode.LEFT_ARROW,
+        'r': Keycode.RIGHT_ARROW
+    }
+
+    if screen == 1:
+        key.set_led(*white)
+        keyboard.send(dir[d.lower()])
+        safe_sleep(0.1)
+        key.set_led(*blue)
+
+
+@pmk.on_press(keys[6])
+def send_up(key):
+    arrow(key, 'u')
+
+
+@pmk.on_press(keys[5])
+def send_down(key):
+    arrow(key, 'd')
+
+
+@pmk.on_press(keys[1])
+def send_left(key):
+    arrow(key, 'l')
+
+
+
+@pmk.on_press(keys[9])
+def send_right(key):
+    arrow(key, 'r')
 
 
 indicator = keys[0]
 comp_indicator = keys[4]
+scr_indicator = keys[12]
 
 while True:
     pmk.update()
+    # mode led
     if mode == 0:
         indicator.led_off()
     if mode == 1:
         indicator.set_led(*white)
     if mode == 2:
         indicator.set_led(*yellow)
-
+    # comp led
     if comp_os == 0:
         comp_indicator.led_off()
     if comp_os == 1:
         comp_indicator.set_led(*white)
     if comp_os == 2:
         comp_indicator.set_led(*yellow)
+    # screen
+    if screen == 0:
+        scr_indicator.led_off()
+    if screen == 1:
+        scr_indicator.set_led(*white)
